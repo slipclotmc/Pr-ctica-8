f14260ce84f630f8566fe9286b99b4bb8ac2e3fc
slipclotmc <slipclotmc8@gmail.com>
2025-11-23 14:45:50 -0600

    Update ejercicio5.c

--- ejercicio5.c
+++ ejercicio5.c
@@ -1,16 +1,21 @@
+/*Velázquez Domínguez Ricardo 
+Práctica 8 - ejercicio 5*/
+
 #include <stdio.h>
 int main ()
 {
-int enteroNumAlumnos = 5;
-float realCalif = 0.0, realPromedio = 0.0;
-printf("\tPromedio de calificaciones\n");
-for (int indice = 0 ; indice < enteroNumAlumnos ; indice++)
-{
-printf("\nIngrese la calificación del alumn %d\n", indice+1);
-scanf("%f",&realCalif);
-realPromedio += realCalif;
-}
-printf("\nEl promedio de las calificaciones ingresadas es: %f\n",
-realPromedio/enteroNumAlumnos);
-return 0;
+    int enteroNumAlumnos = 5;
+    float realCalif = 0.0, realPromedio = 0.0;
+    
+    printf("\tPromedio de calificaciones\n");
+    for (int indice = 0 ; indice < enteroNumAlumnos ; indice++)
+    {
+        printf("\nIngrese la calificación del alumn %d\n", indice+1);
+        scanf("%f",&realCalif);
+        realPromedio += realCalif;
+    }
+    printf("\nEl promedio de las calificaciones ingresadas es: %f\n",
+    realPromedio/enteroNumAlumnos);
+    
+    return 0;
 }
